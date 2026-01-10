b6bb7f07a1624bea972ff38256a4f12143474b51
Juan Carlos Garcia Arriero <juanca755@hotmail.com>
2026-01-10 08:05:05 +0100

    feat: Enhance robots.ts with comprehensive bot rules and crawl-delay

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,24 +1,66 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://almagrosanmiguel.com'
+  
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/', '/_next/'],
+        disallow: [
+          '/api/',
+          '/private/',
+          '/_next/',
+          '/admin/',
+        ],
       },
+      // Google bots - full access
       {
         userAgent: 'Googlebot',
         allow: '/',
-        disallow: ['/api/', '/admin/'],
+        disallow: [
+          '/api/',
+          '/private/',
+        ],
+        crawlDelay: 0,
       },
       {
         userAgent: 'Googlebot-Image',
         allow: '/',
+        disallow: [
+          '/api/',
+          '/private/',
+        ],
+      },
+      // Bing
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/private/',
+        ],
+        crawlDelay: 1,
+      },
+      // Social media bots
+      {
+        userAgent: ['Twitterbot', 'facebookexternalhit'],
+        allow: '/',
+      },
+      // Block bad bots
+      {
+        userAgent: [
+          'AhrefsBot',
+          'SemrushBot',
+          'DotBot',
+          'MJ12bot',
+          'BLEXBot',
+        ],
+        disallow: '/',
       },
     ],
-    sitemap: 'https://almagrosanmiguel.com/sitemap.xml',
-    host: 'https://almagrosanmiguel.com',
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   }
 }
