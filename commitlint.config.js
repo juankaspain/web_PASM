b1cc10a33e6055d0c588e9e74c6ae62429ab4660
Juan Carlos Garcia Arriero <juanca755@hotmail.com>
2026-01-10 03:23:08 +0100

    Add Git hooks, commitlint and prettier configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'chore',
-        'perf',
-        'ci',
-        'build',
-        'revert',
+        'feat',     // Nueva funcionalidad
+        'fix',      // Corrección de bug
+        'docs',     // Documentación
+        'style',    // Formato, punto y coma, etc
+        'refactor', // Refactorización de código
+        'perf',     // Mejora de rendimiento
+        'test',     // Añadir o actualizar tests
+        'build',    // Cambios en build o dependencias
+        'ci',       // Cambios en CI/CD
+        'chore',    // Otras tareas (actualización de dependencias, etc)
+        'revert',   // Revertir un commit anterior
       ],
     ],
     'subject-case': [0],
