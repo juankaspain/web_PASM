c497ea6a282bcfc8b5ddfc3a91ae2fc70b770a24
Juan Carlos Garcia Arriero <juanca755@hotmail.com>
2026-01-10 03:01:40 +0100

    Add robots.ts for search engine crawling control

---
+++ src/app/robots.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/private/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        disallow: '/',
+      },
+    ],
+    sitemap: 'https://almagrosanmiguel.com/sitemap.xml',
+  }
+}
